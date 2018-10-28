408ad35a5c3cb54697d28fe212b532f70072d601
eve <ewashadevsk@gmail.com>
2018-10-28 20:23:59 +0300

    fix: fix make function

--- src/make.js
+++ src/make.js
@@ -1,11 +1,11 @@
 module.exports = function make(...args) {
-	let arr = [];
-	for (i = 0; i < args.length; i++) {
+	const arr = [];
+	for (let i = 0; i < args.length; i++) {
 		if (typeof args[i] == 'function') {
 			return arr.reduce(args[i]);
 		} else {
 			arr.push(args[i]);
-			return make.bind(null, ...args);
 		}
 	}
+	return make.bind(null, ...arr);
 };
